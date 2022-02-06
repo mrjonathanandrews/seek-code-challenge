2026a2bb06a175968ae5d54f9e389d2ec75cbd0a
mrjonathanandrews <92679983+mrjonathanandrews@users.noreply.github.com>
2022-02-06 06:32:24 -0500

    It was at this point that the image toggle started to work. Let's see if I can keep it working as I style the page.

---
+++ barbarian.js
@@ -0,0 +1,52 @@
+import * as THREE from 'https://unpkg.com/three@0.137.5/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.137.5/examples/jsm/loaders/GLTFLoader.js?module';
+
+const canvas = document.getElementById('barbarian-scene-container');
+
+const scene = new THREE.Scene();
+scene.background= new THREE.Color(0x062e06);
+
+const loader = new GLTFLoader();
+loader.load('barbarian.glb', function(gltf) {
+    
+    gltf.scene.position.x = 0; 
+    gltf.scene.position.y = 1
+    gltf.scene.position.z = -2;
+
+    const root = gltf.scene;
+    scene.add(root);
+    root.scale.set(5,5,5);
+}, function(xhr) {
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, undefined, function (error) {
+    console.error(error);
+});
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(2,2,5);
+scene.add(light);
+
+const sizes = {
+    width: 0.625 * window.innerWidth,
+    height: 0.625 * window.innerHeight
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100);
+camera.position.set(0,1,2);
+scene.add(camera);
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.gammaOutput = true;
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+animate();
--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-import * as THREE from 'https://unpkg.com/three@0.137.5/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.137.5/examples/jsm/loaders/GLTFLoader.js?module';
-
-const canvas = document.getElementById('GLTFLoader-scene-container');
-
-const scene = new THREE.Scene();
-
-const loader = new GLTFLoader();
-loader.load('assets/wraith.glb', function(glb) {
-    console.log(glb);
-    const root = glb.scene;
-    scene.add(root);
-    root.scale.set(0.025,0.025,0.025);
-}, function(xhr) {
-    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-}, undefined, function (error) {
-    console.error(error);
-});
-
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(2,2,5);
-scene.add(light);
-
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-};
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100);
-camera.position.set(0,1,2);
-scene.add(camera);
-
-const renderer = new THREE.WebGL1Renderer({
-    canvas: canvas
-});
-
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.gammaOutput = true;
-
-function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+function toggleImg() {
+    
+    const wraith = document.getElementById('wraith-img-container');
+    const barbarian = document.getElementById('barbarian-img-container');
+    const toggleImgButton = document.getElementById('toggle-img-button');
+    
+    if ( wraith.style.display == 'none' ) {
+        barbarian.style.display = 'none';
+        wraith.style.display = 'block';
+        toggleImgButton.value = "Show Barbarian";
+    } else if ( barbarian.style.display == 'none' ) {
+        barbarian.style.display = 'block';
+        wraith.style.display = 'none';
+        toggleImgButton.value = "Show Wraith";        
+    }
 }
-
-animate();
